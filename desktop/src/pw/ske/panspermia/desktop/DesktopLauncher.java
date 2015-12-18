ddbd09a7a15ec60d1f1b105f5a9dcf02cbd2b7f4
Voltasalt <voltasalt@gmail.com>
2015-12-19 00:36:06 +0100

    Initial commit

---
+++ desktop/src/pw/ske/panspermia/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pw.ske.panspermia.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pw.ske.panspermia.Panspermia;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Panspermia(), config);
+	}
+}
